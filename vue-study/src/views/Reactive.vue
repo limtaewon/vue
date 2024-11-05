06aae9e58cef805bc6898d12ad7089c5daa11ef1
임태원/재무서비스개발팀/ND <taewon.lim@nhndooray.com>
2024-11-05 18:55:32 +0900

    reactive

--- vue-study/src/views/Reactive.vue
+++ vue-study/src/views/Reactive.vue
@@ -1,4 +1,5 @@
 <template>
+  <h1 style="margin-bottom: 10px">해당 코드는 노출 버튼을 누를 시 msg가 Updated Message로 바뀌며 빨간색으로 출력되는 것이다</h1>
   <button @click="showH2Content">h2 컨텐츠 노출</button>
   <button @click="unShowH2Content">h2 컨텐츠 숨김</button>
   <h2 ref="msg" v-show="test.isVisible">{{ msg }}</h2>
