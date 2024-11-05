4f26467d05ac50aaeb0961080085884a31b3b35e
임태원/재무서비스개발팀/ND <taewon.lim@nhndooray.com>
2024-11-05 14:23:48 +0900

    reactive

--- vue-study/src/router/index.js
+++ vue-study/src/router/index.js
@@ -10,13 +10,15 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/reactive',
+      name: 'reactive',
+      component: () => import('../views/Reactive.vue'),
     },
+    {
+      path: '/reactive2',
+      name: 'reactive2',
+      component: () => import('../views/Reactive2.vue'),
+    }
   ],
 })
 
